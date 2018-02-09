e880b5a3309e9bca36ac922f7da348ed22deb091
Alan Gomes <alangomes11@gmail.com>
2018-02-08 17:19:47 -0800

    Inicio das refatorações

--- src/tasks/notas.ts
+++ src/tasks/notas.ts
@@ -11,151 +11,115 @@ import * as qauth from '../services/browser/qauth';
 import * as qdiarios from '../services/browser/qdiarios';
 
 interface JobNota {
-    userid: string;
-    matricula: string;
-    senha: string;
-    endpoint: string;
+  userid: string;
+  matricula: string;
+  senha: string;
+  endpoint: string;
 }
 
 export interface JobNotaResult {
-    browser: QBrowser,
-    notas: {
-        alteradas: string[],
-        novas: string[]
-    }
+  browser: QBrowser,
+  notas: {
+    alteradas: string[],
+    novas: string[]
+  }
 }
 
 export function createJob(userid: string, matricula: string, senha: string, endpoint: string): Job {
-    return queue.create('readnotas', {
-        userid: userid,
-        matricula: matricula,
-        senha: senha,
-        endpoint: endpoint
+  return queue.create('readnotas', { userid, matricula, senha, endpoint });
+}
+
+async function createTurma(codigo: string, nome: string): Promise<void> {
+  const turma = await Turma.findOne({ where: { codigo } }) as any;
+  if (!turma) {
+    await Turma.create({ codigo, nome });
+  }
+}
+
+async function createDisciplina(codturma: string, nome: string, professor: string): Promise<string> {
+  const disc = await Disciplina.findOne({ where: { codturma, nome, professor } }) as any;
+  if (!disc) {
+    const { id } = Disciplina.create(
+      { codturma, nome, professor }
+    ) as any as qdiarios.Disciplina;
+    return id;
+  }
+  return disc.id;
+}
+
+async function updateNota(objaluno: any, disciplina: qdiarios.Disciplina, etapa: qdiarios.Etapa, nota: qdiarios.Nota): Promise<void> {
+  const notadb = await Nota.find({
+    where: {
+      descricao: nota.descricao
+    }
+  }) as any;
+  if (!notadb) {
+    const novanota = await Nota.create({
+      etapa: etapa.numero,
+      descricao: nota.descricao,
+      peso: nota.peso,
+      notamaxima: nota.notamaxima,
+      nota: nota.nota,
+      disciplinaid: disciplina.id,
+      userid: objaluno.id
     });
+    await objaluno.addNota(novanota);
+  } else if (nota.peso !== notadb.peso || nota.notamaxima !== notadb.notamaxima || nota.nota !== notadb.nota) {
+    const { notamaxima, peso } = nota;
+    await Nota.update({
+      nota: nota.nota,
+      notamaxima, peso
+    }, {
+        where: {
+          id: notadb.id
+        }
+      });
+  }
 }
 
-function createTurma(codigo: string, nome: string): Promise<any> {
-    return <any> Turma.findOne({ where: { codigo: codigo } })
-        .then(turma => {
-            if (!turma) {
-                return Turma.create({
-                    codigo: codigo,
-                    nome: nome
-                })
-            }
-            return true;
-        });
+async function updateEtapa(objaluno: any, disciplina: qdiarios.Disciplina, etapa: qdiarios.Etapa): Promise<void> {
+  await Promise.all(
+    etapa.notas.map(
+      nota => updateNota(objaluno, disciplina, etapa, nota)
+    )
+  );
 }
 
-function createDisciplina(codturma: string, nome: string, professor: string): Promise<string> {
-    return <any> Disciplina.findOne({ where: { codturma: codturma, nome: nome, professor: professor } })
-        .then((discdb: any) => {
-            if (!discdb) {
-                return Disciplina.create({
-                        codturma: codturma,
-                        nome: nome,
-                        professor: professor
-                    })
-                    .then((res: any) => res.id);
-            }
-            return discdb.id;
-        });
+export async function retrieveData(browser: QBrowser, matricula: string): Promise<JobNotaResult> {
+  const jobresult = {
+    browser: browser
+  };
+  const objaluno = await Usuario.find({ where: { matricula: matricula } }) as any;
+  const disciplinas = await qdiarios.getDisciplinas(browser);
+  for (let disc of disciplinas) {
+    await createTurma(disc.turma, 'Turma ' + disc.turma);
+    const id = await createDisciplina(disc.turma, disc.nome, disc.professor);
+    if (await objaluno.hasDisciplina(id)) {
+      await objaluno.addDisciplina(id);
+    }
+    disc.id = id;
+    await Promise.all(
+      disc.etapas.map(etapa => updateEtapa(objaluno, disc, etapa))
+    );
+  }
+  return jobresult as any;
 }
 
-function updateNota(objaluno: any, disciplina: qdiarios.Disciplina, etapa: qdiarios.Etapa, nota: qdiarios.Nota): Promise<any> {
-    return <any> Nota.find({
-            where: {
-                descricao: nota.descricao
-            }
-        })
-        .then((notadb: any) => {
-            if (!notadb) {
-                return Nota.create({
-                    etapa: etapa.numero,
-                    descricao: nota.descricao,
-                    peso: nota.peso,
-                    notamaxima: nota.notamaxima,
-                    nota: nota.nota,
-                    disciplinaid: disciplina.id,
-                    userid: objaluno.id
-                })
-                .then((novanota: any) => objaluno.addNota(novanota).then(() => novanota))
-            }
-            if (nota.peso !== notadb.peso || nota.notamaxima !== notadb.notamaxima || nota.nota !== notadb.nota) {
-                return Nota.update({
-                    nota: nota.nota,
-                    notamaxima: nota.notamaxima,
-                    peso: nota.peso
-                },
-                {
-                    where: {
-                        id: notadb.id
-                    }
-                });
-            }
-            return notadb;
-        })
+export async function atualizaNotas(): Promise<void> {
+  const users = await Usuario.all() as any;
+  users.forEach(user => {
+    const job = createJob(user.id, user.matricula, cipher.decipher(user.password, configs.cipher_pass), user.endpoint)
+      .removeOnComplete(true).events(false).ttl(2.4e5 /* 4min */).save();
+  });
 }
 
-function updateEtapa(objaluno: any, disciplina: qdiarios.Disciplina, etapa: qdiarios.Etapa): Promise<any[]> {
-    const proms = etapa.notas.map(nota => updateNota(objaluno, disciplina, etapa, nota));
-    return Promise.all(proms);
-}
+queue.process('readnotas', configs.update_queue_size, async (jobinfo, done) => {
+  const job: JobNota = jobinfo.data;
 
-export function retrieveData(browser: QBrowser, matricula: string): Promise<JobNotaResult> {
-    const jobresult = {
-        browser: browser
-    };
-    let objaluno;
-    return <any> Usuario.find({ where: { matricula: matricula } })
-        .then((user: any) => objaluno = user)
-        .then(() => qdiarios.getDisciplinas(browser))
-        .then(disciplinas => {
-            let sequencia = Promise.resolve();
-            disciplinas.forEach(disc => {
-                sequencia = sequencia
-                    .then(() => createTurma(disc.turma, 'Turma ' + disc.turma))
-                    .then(() => createDisciplina(disc.turma, disc.nome, disc.professor))
-                    .then(discid => 
-                        objaluno.hasDisciplina(discid)
-                            .then(has => {
-                                if (!has) {
-                                    return objaluno.addDisciplina(discid)
-                                        .then(() => discid);
-                                }
-                                return discid;
-                            })
-                    )
-                    .then(discid => {
-                        disc.id = discid;
-                        const proms = disc.etapas.map(etapa => updateEtapa(objaluno, disc, etapa));
-                        return <any> Promise.all(proms);
-                    });
-            })
-            return sequencia;
-        })
-        .then(() => jobresult);
-}
-
-export function atualizaNotas() {
-    return Usuario.all()
-        .then((users: any[]) => {
-            users.forEach(user => {
-                const job = createJob(user.id, user.matricula, cipher.decipher(user.password, configs.cipher_pass), user.endpoint)
-                    .removeOnComplete(true).events(false).ttl(2.4e5 /* 4min */).save();
-            });
-            return users;
-        });
-}
-
-queue.process('readnotas', configs.update_queue_size, function (jobinfo, done) {
-    const job: JobNota = jobinfo.data;
-    qauth.login(job.endpoint, job.matricula, job.senha)
-        .then(browser => retrieveData(browser, job.matricula))
-        .then(result => result.browser.exit())
-        .then(results => done());
+  const browser = await qauth.login(job.endpoint, job.matricula, job.senha);
+  await retrieveData(browser, job.matricula);
+  await browser.exit();
+  done();
 });
-
-
 
 export { queue };
