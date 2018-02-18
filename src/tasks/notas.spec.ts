09af743fbcdfada5808179eaea2e959c4aaa9fbc
Alan Gomes <alangomes11@gmail.com>
2018-02-18 20:15:00 -0300

    Finalização dos testes de job

--- src/tasks/notas.spec.ts
+++ src/tasks/notas.spec.ts
@@ -1,7 +1,12 @@
+import { QBrowser } from './../services/driver/qbrowser';
 import { Disciplina } from './../models/disciplina';
 import { Nota } from './../models/nota';
 import { Usuario } from './../models/usuario';
 import * as auth from '../services/auth/authenticate';
+import * as qauth from '../services/browser/qauth';
+import * as qdiarios from '../services/browser/qdiarios';
+import * as queue from './queue';
+import * as cipher from '../services/cipher/cipher';
 import { Turma } from '../models/turma';
 import { NotasTask } from './notas';
 
@@ -20,10 +25,14 @@ describe('NotasTask', () => {
   }
 
   beforeAll(async done => {
+    spyOn(NotasTask, 'updateRemote').and.returnValue(
+      Promise.resolve()
+    );
     Promise.all([
       limpar(),
       (async () => {
         usuario = await auth.login('http://localhost:9595', 'test', 'pass');
+        (NotasTask.updateRemote as jasmine.Spy).and.callThrough();
       })()
     ]
     ).then(done).catch(done.fail);
@@ -284,6 +293,151 @@ describe('NotasTask', () => {
         expect(ret1).toEqual([jasmine.any(Nota), 'nova'] as any);
         expect(ret2).toEqual([jasmine.any(Nota), 'alterada'] as any);
         expect(ret3).toEqual([jasmine.any(Nota), 'normal'] as any);
+        done();
+      } catch (e) {
+        done.fail(e);
+      }
+    });
+
+  });
+
+  describe('updateAll()', () => {
+
+    it('deve filtrar os resultados retornados pelo updateDisciplina()', async done => {
+      try {
+        let i = 0;
+        spyOn(NotasTask, 'updateDisciplina').and.callFake(async () => {
+          i++;
+          switch (i) {
+            case 1:
+              return [[{ tst: 1 }, 'nova'], [{ tst: 4 }, 'normal']];
+            case 2:
+              return [[{ tst: 2 }, 'alterada'], [{ tst: 5 }, 'nova']];
+            case 3:
+              return [[{ tst: 3 }, 'normal'], [{ tst: 6 }, 'alterada']];
+          }
+        });
+
+        const ret = await NotasTask.updateAll(usuario, [
+          { disciplina: 1 },
+          { disciplina: 2 },
+          { disciplina: 3 },
+        ] as any);
+        expect(NotasTask.updateDisciplina).toHaveBeenCalledTimes(3);
+        expect(NotasTask.updateDisciplina).toHaveBeenCalledWith(usuario, { disciplina: 1 });
+        expect(NotasTask.updateDisciplina).toHaveBeenCalledWith(usuario, { disciplina: 2 });
+        expect(NotasTask.updateDisciplina).toHaveBeenCalledWith(usuario, { disciplina: 3 });
+        expect(ret).toEqual({
+          notas: {
+            alteradas: [{ tst: 2 }, { tst: 6 }] as any,
+            novas: [{ tst: 1 }, { tst: 5 }] as any,
+          }
+        });
+        done();
+      } catch (e) {
+        console.error(e);
+        done.fail(e);
+      }
+    });
+
+  });
+
+  describe('updateRemote()', () => {
+
+    let browser: QBrowser;
+    beforeAll(async done => {
+      try {
+        browser = await qauth.login('http://localhost:9595', 'test', 'pass');
+        done();
+      } catch (e) {
+        done.fail(e);
+      }
+    });
+
+    beforeEach(() => {
+      spyOn(NotasTask, 'updateAll').and.returnValue(Promise.resolve({ tst: 1 }));
+      spyOn(qdiarios, 'getDisciplinas').and.returnValue(Promise.resolve({ tst: 2 }));
+    });
+
+    afterAll(done => {
+      browser.exit().then(done).catch(done.fail);
+    });
+
+    it('deve atualizar as notas do aluno', async done => {
+      try {
+        spyOn(Usuario, 'findOne').and.callThrough();
+        await NotasTask.updateRemote(browser, 'test');
+
+        expect(Usuario.findOne).toHaveBeenCalledWith({ where: { matricula: 'test' } });
+        expect(qdiarios.getDisciplinas).toHaveBeenCalledWith(browser);
+        expect(NotasTask.updateAll).toHaveBeenCalledWith(jasmine.any(Usuario), { tst: 2 });
+        done();
+      } catch (e) {
+        done.fail(e);
+      }
+    });
+
+    it('se o aluno não existir, não deve atualizar as notas', async done => {
+      try {
+        spyOn(Usuario, 'findOne').and.returnValue(Promise.resolve(null));
+        await NotasTask.updateRemote(browser, 'test');
+
+        expect(qdiarios.getDisciplinas).not.toHaveBeenCalled();
+        expect(NotasTask.updateAll).not.toHaveBeenCalled();
+        done();
+      } catch (e) {
+        done.fail(e);
+      }
+    });
+
+  });
+
+  describe('scheduleUpdate()', () => {
+
+    beforeEach(() => {
+      spyOn(queue, 'create').and.callThrough();
+      spyOn(cipher, 'decrypt').and.returnValue('decrypted');
+    });
+
+    it('deve buscar todos os alunos', async done => {
+      try {
+        spyOn(Usuario, 'all').and.returnValue(Promise.resolve([]));
+        await NotasTask.scheduleUpdate();
+
+        expect(Usuario.all).toHaveBeenCalled();
+        done();
+      } catch (e) {
+        done.fail(e);
+      }
+    });
+
+    it('deve criar um job para cada aluno', async done => {
+      try {
+        spyOn(Usuario, 'all').and.returnValue(Promise.resolve([
+          { id: 'a', matricula: 't1', password: 'p1', endpoint: 'e1' },
+          { id: 'b', matricula: 't2', password: 'p2', endpoint: 'e2' },
+          { id: 'c', matricula: 't3', password: 'p3', endpoint: 'e3' }
+        ]));
+        await NotasTask.scheduleUpdate();
+
+        expect(queue.create).toHaveBeenCalledWith('readnotas', {
+          userid: 'a',
+          matricula: 't1',
+          senha: 'decrypted',
+          endpoint: 'e1'
+        });
+        expect(queue.create).toHaveBeenCalledWith('readnotas', {
+          userid: 'b',
+          matricula: 't2',
+          senha: 'decrypted',
+          endpoint: 'e2'
+        });
+        expect(queue.create).toHaveBeenCalledWith('readnotas', {
+          userid: 'c',
+          matricula: 't3',
+          senha: 'decrypted',
+          endpoint: 'e3'
+        });
         done();
       } catch (e) {
         done.fail(e);
